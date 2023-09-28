fb9ff05246de69de146ba1c87d7a83f7e951da6f
Yash Pathak <144787943+YashPathak1110@users.noreply.github.com>
2023-09-28 19:29:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Form from './Componant/Form';
+function App() {
+  return (
+    <div className="App">
+      <div className='container'>
+          <div className='card'>
+            <Form/>
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
